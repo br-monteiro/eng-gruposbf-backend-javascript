8b8648bd1a437b72fbc7301d7a02708c28c1ff35
Edinho Monteiro <bruno.monteirodg@gmail.com>
2022-12-11 16:20:14 +0000

    refactor: use logger implentation instead console.log

--- src/server.js
+++ src/server.js
@@ -1,14 +1,15 @@
 #!/usr/bin/env node
 
 const app = require('./index')
+const logger = require('./logger')('server')
 
 const port = process.env.PORT || 3000
 
 app.listen(port, async error => {
   if (error) {
-    console.error('unable to listen for connections', error)
+    logger.error('unable to listen for connections', error)
     process.exit(10)
   }
 
-  console.info(`server is listening on port ${port}`)
+  logger.info(`server is listening on port ${port}`)
 })
