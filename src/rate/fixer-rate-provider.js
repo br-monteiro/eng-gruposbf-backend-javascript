5de7a41c5f91f954e93d0cc088c2a15c7e241ecb
Edinho Monteiro <bruno.monteirodg@gmail.com>
2022-12-13 05:21:10 +0000

    feat: add implementation for Fixer rate provider

---
+++ src/rate/fixer-rate-provider.js
@@ -0,0 +1,51 @@
+const RateProvider = require('./rate-provider')
+const logger = require('../logger')('rate/fixer-rate-provider')
+
+class FixerRateProvider extends RateProvider {
+  constructor(baseUrl, apikey) {
+    super(baseUrl, apikey)
+  }
+
+  buildUrl(currencyBase) {
+    return `${this.getBaseUrl()}?base=${currencyBase}`
+  }
+
+  /**
+   * @param { string } currencyBase - The currency base value
+   * @returns { Promise }
+   */
+  async fetch (currencyBase) {
+    return fetch(this.buildUrl(currencyBase), {
+      headers: {
+        apikey: this.getApikey()
+      }
+    })
+      .then(res => res.json())
+      .catch(error => {
+        logger.error('error to try fetch the rates from Fixer API', error)
+        return {
+          status: 'error'
+        }
+      })
+  }
+
+  /**
+   * @param { string } currencyBase - The currency base avlue
+   * @returns { Promise<import("./rate-provider").CurrencyRateMap> }
+   */
+  async map (data) {
+    if (!data || data?.status === 'error') {
+      logger.error('error to try map data to CurrencyRateMap')
+      return {}
+    }
+
+    return {
+      base: data.base,
+      rateDate: new Date(data.timestamp),
+      requestDate: new Date(),
+      rates: new Map(Object.entries(data.rates))
+    }
+  }
+}
+
+module.exports = FixerRateProvider
