0192a2a7fa7cf9180175f1f4eb568fc49deca732
Edinho Monteiro <bruno.monteirodg@gmail.com>
2022-12-14 03:10:49 +0000

    feat: add cache implementation (#7)
    * refactor: change cache hit condition
    
    * feat: add cache manager implementation
    
    * feat: add first settings
    
    * feat: add redis client implementation
    
    * feat: add redis implementation
    
    * chore: add redis and mergician dependencies
    
    * chore: update package-lock file
    
    * feat: add first version of docker-comporse file

---
+++ src/cache/redis-client.js
@@ -0,0 +1,25 @@
+const logger = require('../logger')('chache/redis-client')
+const { cache } = require('../configs')
+const { createClient } = require('redis')
+
+const client = createClient()
+
+client.on('error', (error) => logger.error('redis client error', error))
+client
+  .connect()
+  .then(() => logger.info('redis connected successfully'))
+  .catch(() => logger.error('error to try connect with redis'))
+
+function get (key) {
+  return client.get(key)
+}
+
+function set (key, value) {
+  return client.setEx(key, cache.ttl, value)
+}
+
+module.exports = {
+  client,
+  get,
+  set
+}
---
+++ src/configs/index.js
@@ -0,0 +1,20 @@
+const defaultSettings = require('./default')
+const mergician = require('mergician')
+
+const env = process.env.NODE_ENV || 'development'
+
+function mergeSettings(env) {
+  let settings = {}
+
+  if (env === 'development') {
+    settings = require('./development')
+  }
+
+  if (env === 'production') {
+    settings = require('./production')
+  }
+
+  return mergician(defaultSettings, settings)
+}
+
+module.exports = mergeSettings(env)
