2b2ba6e70fa55dd17fbd565b9c7a2ce0d04d8b22
Edinho Monteiro <bruno.monteirodg@gmail.com>
2022-12-14 01:16:58 +0000

    feat: add rate manager implementation

---
+++ src/rate/rate-manager.js
@@ -0,0 +1,48 @@
+const logger = require('../logger')('rate/rate-manager')
+
+class RateManager {
+  #poolRateProviders
+  #cacheManager
+
+  /**
+   * @param { Array<RateProvider> } poolRateProviders
+   */
+  constructor(poolRateProviders, cacheManager) {
+    this.#poolRateProviders = poolRateProviders
+    this.#cacheManager = cacheManager
+  }
+
+  async #getRateFromProvider(currencyBase, currencyDestination) {
+    const providers = this.#poolRateProviders.map(async (provider) => {
+      const result = await provider.fetch(currencyBase)
+      return this.#poolRateProviders.map(result)
+    })
+
+    return Promise.any(providers)
+      .then(async (result) => {
+        const rateValue = result?.rates?.get(currencyDestination.getId()) || 0
+
+        await this.#cacheManager.updateRates(result)
+        return rateValue
+      })
+      .catch(error => {
+        logger.error('error to get the rates from provider', error)
+        return 0
+      })
+  }
+
+  async getRate(currencyBase, currencyDestination) {
+    return this.#cacheManager
+            .get(currencyBase, currencyDestination)
+            .then(async (result) => {
+              if (result.status === this.#cacheManager.HIT) {
+                return result.value
+              }
+
+              return this.#getRateFromProvider(currencyBase, currencyDestination)
+            })
+
+  }
+}
+
+module.exports = RateManager
