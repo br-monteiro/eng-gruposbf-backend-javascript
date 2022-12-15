811e029be6bc3ea2e139c6837c6e6b3e4f4bf53e
Edinho Monteiro <bruno.monteirodg@gmail.com>
2022-12-15 02:08:00 +0000

    feat: add converter manager

---
+++ src/converter/converter-manager.js
@@ -0,0 +1,57 @@
+const logger = require('../logger')('converter/converter')
+
+class ConverterManager {
+  STATUS_ERROR = 'error'
+  STATUS_SUCCESS = 'success'
+
+  #currenciesContainer
+  #rateManager
+
+  constructor (currenciesContainer, rateManager) {
+    this.#currenciesContainer = currenciesContainer
+    this.#rateManager = rateManager
+  }
+
+  #buildConvertResultObject(status) {
+    return {
+      status,
+      rates: {}
+    }
+  }
+
+  async #buildConvertResultMap(rates, value) {
+    rates.reduce((ratesMap, rate) => {
+      const currency = this.#currenciesContainer.getCurrency(rate.origin)
+
+      if (currency) {
+        ratesMap[rate.destination] = currency.convert(value, rate.value)
+      }
+
+      return ratesMap
+    }, this.#buildConvertResultObject(this.STATUS_SUCCESS))
+  }
+
+  async convert (currencyBase, value) {
+    if (!this.#currenciesContainer.hasCurrency(currencyBase)) {
+      logger.error(`the currency ${currencyBase} is not allowed to be converted`)
+
+      return this.#buildConvertResultObject(this.STATUS_ERROR)
+    }
+
+    const rates = this.currenciesContainer.getAllIds()
+      .reduce(async (accRates, id) => {
+        if (id === currencyBase) return accRates
+        accRates.push(this.#rateManager.getRate(currencyBase, id))
+        return accRates
+      }, [])
+
+    return Promise.all(rates)
+      .then(async (result) => await this.#buildConvertResultMap(result))
+      .catch(error => {
+        logger.error(`error to try convert ${currencyBase} ${value}`, error)
+        return this.#buildConvertResultObject(this.STATUS_ERROR)
+      })
+  }
+}
+
+module.exports = ConverterManager
