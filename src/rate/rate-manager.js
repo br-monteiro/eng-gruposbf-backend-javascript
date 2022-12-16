777dc0f0ece9587c3224b43b80c44741b11de795
Edinho Monteiro <bruno.monteirodg@gmail.com>
2022-12-16 02:25:36 +0000

    fix: call map method correctly

--- src/rate/rate-manager.js
+++ src/rate/rate-manager.js
@@ -15,7 +15,7 @@ class RateManager {
   async #getRateFromProvider(currencyBase, currencyDestination) {
     const providers = this.#poolRateProviders.map(async (provider) => {
       const result = await provider.fetch(currencyBase)
-      return this.#poolRateProviders.map(result)
+      return provider.map(result)
     })
 
     return Promise.any(providers)
