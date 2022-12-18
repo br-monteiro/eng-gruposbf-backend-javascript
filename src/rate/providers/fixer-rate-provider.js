e974f4e31b56554bd8dec088240b00a49b4ebb7e
Edinho Monteiro <bruno.monteirodg@gmail.com>
2022-12-18 04:15:10 +0000

    chore: add logs level info on strategy points

--- src/rate/providers/fixer-rate-provider.js
+++ src/rate/providers/fixer-rate-provider.js
@@ -22,7 +22,10 @@ class FixerRateProvider extends RateProvider {
         'Accept-Encoding': 'gzip,deflate,compress'
       }
     })
-      .then(res => res.data)
+      .then(res => {
+        logger.info(`get rates from FIXER provider for currency base ${currencyBase}`)
+        return res.data
+      })
       .catch(error => {
         logger.error('error to try fetch the rates from Fixer API', error)
         return {
